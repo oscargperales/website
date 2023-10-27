87c473cec99df0efdee8e635ed8557faca67212d
iamoscargp <oscar.g.perales@outlook.com>
2023-10-26 23:23:06 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import ReactGA from 'react-ga';
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills/Skills";
+import Blogs from "./pages/Blogs"
+import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./components/Footer/Footer";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react'
+
+if (typeof process.env.REACT_APP_TRACKING_ID !== 'undefined') {
+    ReactGA.initialize(process.env.REACT_APP_TRACKING_ID)
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <NavBar/>
+                <ScrollToTop/>
+                <Routes>
+                    <Route path={"/portfolio"} exact element={<Home/>}/>
+                    <Route path={"/projects"} exact element={<Projects/>}/>
+                    <Route path={"/blogs"} exact element={<Blogs/>}/>
+                    <Route path={"/skills"} exact element={<Skills/>}/>
+                </Routes>
+                <Footer/>
+            </div>
+        </Router>
+
+    )
+
 }
 
 export default App;
