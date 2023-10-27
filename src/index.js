05db1952b09cd81a10dabfde3c15eace2b088dfb
iamoscargp <oscar.g.perales@outlook.com>
2023-10-27 13:38:25 -0500

    main

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import "./index.css";
+import './index.css';
 
-
-ReactDOM.render(
+const root = document.getElementById('root');
+const rootInstance = createRoot(root);
+rootInstance.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
