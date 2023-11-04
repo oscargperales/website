6d823217449c20ee010c43c4f8e2dd624d5c664a
iamoscargp <oscar.g.perales@outlook.com>
2023-11-04 09:11:50 -0500

    added rangers

--- src/assets/configs/projectConfig.js
+++ src/assets/configs/projectConfig.js
@@ -1,9 +1,25 @@
 import cover from "../images/jcole.jpeg";
 import web from "../images/web.jpeg";
+import rangers from "../images/rangers.jpeg";
 import { BsGithub } from "react-icons/bs";
 
 const projectConfig = [
   {
+    id: "project-3",
+    title: "2023 World Series",
+    completed: true, // This project is complete
+      links: [
+        {
+          name: "repo",
+          url: "https://github.com/oscargperales/World-Series-23",
+          icon: <BsGithub />,
+        },
+      ],
+    image: rangers,
+    description: "Performance analysis of the 2023 World Series in which the Texas Rangers claimed victory",
+    target: "_blank",
+  },
+    {
     id: "project-2",
     title: "J Cole Album Analysis",
     completed: false, // This project is in progress
